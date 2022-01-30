874fa7ef3e4eb043b3c096695ed7b7122628285d
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2022-01-30 11:06:01 -0800

    WIP: update createRunLine

--- tmp.js
+++ tmp.js
@@ -1,28 +1,67 @@
-// var eval = function(inputItems, outputItems, line_desc) {
+// Line specification describing the following conveyor belt:
+//
+//   \i1/r0
+//   |s0|s1|s2|
+//         /o0\
+//
+//   \i0/    : 0th input chute
+//   /o0\    : 0th output chute
+//   r0      : 0th robot
+//   s0,s1,s2: 0th, 1st, and 2nd conveyor segments
+//
+var conveyorLineSpec = {
+  segments: [
+    {
+      id: 0,
+      to: 1,
+      from: null,
+    },
+    {
+      id: 0,
+      to: 1,
+      from: null,
+    },
+    {
+      id: 0,
+      to: 1,
+      from: null,
+    },
+  ],
+  robots: [
+    {
+      id: 0,
+      loc: 1,
+    },
+  ],
+  input_method: {
+    type: "chute",
+    to: 0, // segment_id
+  },
+  output_method: {
+    type: "chute",
+    from: 2,
+  },
+};
 
-//     //
+var createRunLine = function (lineSpec) {
+  var firstSegment = lineSpec.segments[0];
+  var runLine = function (inputQueue) {
+    var outputQueue = [];
+    var duration = 0;
+    return duration;
+  };
+  return runLine;
+};
 
-//     // var state = {
-//     //     item: null,
-//     //     duration: 0,
-//     // }
+var runLine = createRunLine(conveyorLineSpec);
+var testInputQueue = [];
+var outputDuration = runLine(testInputQueue);
 
-//     // var duration = 1;
+var result = {
+  conveyorLineSpec: conveyorLineSpec,
+  createRunLine: createRunLine,
+  runLine: runLine,
+  createRunLine: createRunLine,
+};
 
-//     while (inputItems.length() > 0) {
-//         if (state.item !== null && duration === 0) {
-//             outputItems.push_back(state.item);
-//         }
-//     }
-
-//     return 1;
-// }
-
-var state = {
-    item: null,
-    duration: 0,
-}
-
-var duration = sample(Poisson({mu: 1}));
-
-console.log(duration)
+result;
