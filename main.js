ed2b9c619d97c93dc0c7f6903294a1efa2410802
Mike Chung <mchung@vicarious.com>
2022-02-04 15:10:55 -0800

    Update comment

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+/**
+ * webppl-linesim
+ * --------------
+ *
+ * How to run this code:
+ * 1. Install nodejs v15.5.1, e.g., using [nvm](https://github.com/nvm-sh/nvm)
+ * 2. Run `npm install`
+ * 3. Run `./node_modules/.bin/webppl main.js`
+ *
+ * For gentle introduction to probabilistic programming, check out
+ * http://adriansampson.net/doc/ppl.html
+ *
+ */
+
 // Let's say we are interested in the expected throughput of a line. In this demo, I
 // demonstrate how to use a probabilistic programming language to (1) build a
 // statistical model of a line and (2) use the model to infer the throughput
